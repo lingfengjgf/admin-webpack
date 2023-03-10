a119c269033159cde380d93c43fa4b643bb2c09f
lingfengjgf <lingfengjgf@163.com>
2023-03-10 16:46:12 +0800

    admin项目组装

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <button @click="user.add()">{{ user.count }}</button>
+<template lang="">
+    <!-- <div>
+        <h1>{{ msg }}</h1>
+
+        <button @click="user.add()">count: {{ user.count }}</button>
+    </div> -->
     <router-view></router-view>
-  </div>
+    
 </template>
-
 <script lang="ts" setup>
-import { ref, Ref } from 'vue';
-import { useUserStore } from '@/store/index';
-// export default ({
-//   setup(){
-    const msg:Ref<number> = ref(1);
 
-    setInterval(()=>{
-      msg.value++;
-    }, 1000)
+import { defineComponent, ref, Ref } from "vue"
 
-    const user = useUserStore();
-    // return { msg };
-//   }
+// import { useUserStore } from "./stores/user";
+// const user = useUserStore()
+
+// export default defineComponent({
+//     setup() {
+//         // const msg: Ref<number> = ref(1)
+//         const msg = ref(1)
+//         setInterval(() => {
+//             msg.value++
+//         }, 1000)
+//         return { msg }
+//     }
 // })
+
+// const msg = ref(1)
+
 </script>
 
 <style scoped>
-  h1{
-    color: red;
-  }
+/* h1 {
+    color: blue;
+} */
 </style>
+    
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { str } from "@/add";
 
 import { createApp } from "vue";
 import App from "@/App.vue";
-import router from "./routes";
+import router from "./router";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 
 import './style.css';
 
 createApp(App)
 .use(router)
 .use(createPinia())
+.use(createI18n({
+  legacy:false,
+  locale:'en'
+}))
 .mount("#app");
 
 console.log('Hello Webpack!');
